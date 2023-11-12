a823724dafef9b415aedf36a730990e39a5e967b
Pramuditha Jayasinghe <pramuditha.niroshan@circles.asia>
2023-11-12 16:06:33 +0530

    Add page Galary

---
+++ src/components/Gallary/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./index.scss";
+
+const Gallery = ({images}) => {
+    console.log(images);
+  return (
+    <>
+      <h1>Image Gallery</h1>
+      <p>Recent occations in our organization</p>
+      <br />
+      <div className="grid-container">
+        {
+            images.length > 0 && images.map(image => {
+                return (<div className="grid-item">
+                    <img src={image.download_url} alt="image"/>
+                    <p>{image.author}</p>
+                </div>)
+            })
+        }
+        
+      </div>
+    </>
+  );
+};
+
+export default Gallery;
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Gallery from "../../components/Gallary";
 
 const Portfolio = () => {
-  return (
-    <div>portfolio</div>
-  )
-}
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get("https://picsum.photos/v2/list").then((response) => {
+      setData(response.data);
+    });
+  }, []);
+  return data && <Gallery images={data} />;
+};
 
-export default Portfolio
+export default Portfolio;
